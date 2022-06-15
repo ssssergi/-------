ff8a148c552b8528e44c716d67c9b6053fe834c3
ssssergi <ssssergissergi@gmail.com>
2022-06-15 18:57:12 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//string
+// let hello = 'Привет'
+// console.log(hello.length)
+// console.log(hello[hello.length -1])
+// console.log(hello + ' Медведь')
+
+
+//числа
+// let a = 12
+// let b = 11 
+// console.log(a%b)
+
+
+
+//массивы
+// let count = 1
+// let count2 = 2
+// let count3 = 3
+// let numbers = [count, count2, count3]
+// console.log([count] * [count2])
+// console.log(numbers[numbers.length - 1])
+
+
+// //циклы
+// for (let i = 0; i <= 50; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 0; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
+// }
+
+// let numbers = [1, 2, 3] 
+
+
+//функции
+// function numbers() {
+//     for (let);
+        
+//     }
+// }
---
+++ function.js
@@ -0,0 +1,21 @@
+// function numbers(privet) {
+//     if (privet => 18) {
+//         console.log('ДА')
+//     } else {
+//         console.log('НЕТ')
+//     }
+// }
+
+// numbers(16)
+
+
+// function greet(name) {
+//     console.log('Привет от Кристины воняет ', name)
+// }
+
+// greet('говной')
+// greet('немытой голубкой')
+
+for (let i = 50; i > 0; i--) {
+    console.log(i);
+}
